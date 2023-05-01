e43fa7ab2e7104e212b377bb9c97e9979e42c6ac
KacperW <kwoznicki10@gmail.com>
2023-05-01 14:53:03 +0200

    AboutConcept

--- components/About/About.js
+++ components/About/About.js
@@ -3,6 +3,9 @@ import WhatsApp from "../../public/whats.svg";
 import Instagram from "../../public/insta.svg";
 import Messenger from "../../public/messenger.svg";
 import Git from "../../public/git.svg";
+import TechImg from "../../public/tech.svg"
+import LangImg from "../../public/lang.svg"
+import SkillsImg from "../../public/skills.svg"
 import Image from "next/image";
 import Link from "next/link";
 
@@ -38,7 +41,7 @@ function About() {
           })}
         </section>
         <Link href="" legacyBehavior>
-          <a href="">
+          <a>
             <Image src={Messenger} alt="Messenger" target="_blank" />
           </a>
         </Link>
@@ -69,6 +72,7 @@ function About() {
         <section className={styles.rightSec}>
           
           <div className={styles.skllsLeftSec}>
+            <div>
             {skills.map((char, index) => {
               if (char === " ") {
                 return <span key={index}>&nbsp;</span>;
@@ -80,6 +84,10 @@ function About() {
                 );
               }
             })}
+            </div>
+            <div>
+            <Image src={SkillsImg} alt="skills" />
+            </div>
           </div>
           <div className={styles.skills}>
             <ul>
@@ -108,6 +116,7 @@ function About() {
             </ul>
           </div>
           <div className={styles.skllsLeftSec}>
+            <div>
             {tech.map((char, index) => {
               if (char === " ") {
                 return <span key={index}>&nbsp;</span>;
@@ -119,6 +128,10 @@ function About() {
                 );
               }
             })}
+            </div>
+            <div>
+            <Image src={TechImg} alt="tech" />
+            </div>
           </div>
           <div className={styles.skills}>
             <ul>
@@ -150,6 +163,7 @@ function About() {
             </ul>
           </div>
           <div className={styles.skllsLeftSec}>
+            <div>
             {lang.map((char, index) => {
               if (char === " ") {
                 return <span key={index}>&nbsp;</span>;
@@ -161,6 +175,10 @@ function About() {
                 );
               }
             })}
+            </div>
+            <div>
+             <Image src={LangImg} alt="lang" />
+            </div>
           </div>
           <div className={styles.skills}>
             <ul>
