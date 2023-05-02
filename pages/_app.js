2ec50fcb1fecf170f493ac9a66c32f508dbae553
KacperW <kwoznicki10@gmail.com>
2023-05-02 22:10:31 +0200

    TestOg

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import "../styles/globals.scss";
 import Layout from "../components/Layout/Layout";
+import CustomHelmet from "../components/utils/CustomHelmet";
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <CustomHelmet />
     <Layout>
       <Component {...pageProps} />
     </Layout>
