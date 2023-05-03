457d40cb794e6980e7e77018724c8017a31170b8
KacperW <kwoznicki10@gmail.com>
2023-05-03 23:02:40 +0200

    HotFixHead

--- components/utils/CustomHelmet.js
+++ components/utils/CustomHelmet.js
@@ -10,12 +10,7 @@ const CustomHelmet = () => {
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="theme-color" content="#000000" />
       <meta name="color-scheme" content="dark" />
-      <meta property="og:title" content="Kacper Woźnicki" />
-      <meta property="og:type" content="website" />
-      <meta property="og:description" content="Front-end Developer Portfolio" />
-      <meta property="og:image" content="/img.jpg" />
-      <meta property="og:locale" content="en - EN" />
-      <meta property="og:url" content="https://kwoznicki.vercel.app" />
+      
       <link rel="canonical" href="https://kwoznicki.vercel.app" />
       <link rel="icon" type="image.png" href="/favicon.png" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
