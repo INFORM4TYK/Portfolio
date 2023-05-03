83b624a1c6db12db31c13dc4e605d384c63769bb
KacperW <kwoznicki10@gmail.com>
2023-05-03 22:35:53 +0200

    HotFix

--- components/utils/CustomHelmet.js
+++ components/utils/CustomHelmet.js
@@ -8,7 +8,6 @@ const CustomHelmet = () => {
       <meta name="keywords" content="portfolio,frontend,developer,it" />
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="theme-color" content="#000000" />
       <meta name="color-scheme" content="dark" />
       <meta property="og:title" content="Kacper Woźnicki" />
