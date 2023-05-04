80144d0afedf31cd05dde69b39770d6cd05009d3
KacperW <kwoznicki10@gmail.com>
2023-05-04 13:01:55 +0200

    HotFix

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,10 @@
 import Contact from "../components/ContactForm/Contact";
 import About from "../components/About/About";
-import Head from "next/head";
+
 export default function Home() {
   return (
     <>
-    <Head>
-    <html lang="en" />
-      <title>Kacper Woźnicki</title>
-      <meta name="description" content="Front-end Developer Kacper Woźnicki" />
-      <meta name="keywords" content="portfolio,frontend,developer,it" />
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="theme-color" content="#000000" />
-      <meta name="color-scheme" content="dark" />
-      <meta property="og:url" content="https://kwoznicki.vercel.app" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="Portfolio" />
-      <meta property="og:description" content="Front-end Developer Kacper Woźnicki" />
-      <meta property="og:image" content="https://kwoznicki.vercel.app/img.jpg" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="kwoznicki.vercel.app" />
-      <meta property="twitter:url" content="https://kwoznicki.vercel.app" />
-      <meta name="twitter:title" content="Portfolio" />
-      <meta name="twitter:description" content="Portfoli Kacper Woźnicki" />
-      <meta name="twitter:image" content="https://kwoznicki.vercel.app/img.jpg" />
-      <link rel="canonical" href="https://kwoznicki.vercel.app" />
-      <link rel="icon" type="image.png" href="/favicon.png" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Lato&family=Roboto:wght@100;400;700;900&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
+     
       <About />
       <Contact />
     </>
